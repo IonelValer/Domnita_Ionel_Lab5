d3d633fe343ff2c84458553af20950d5d56d89f4
Ionel Valer <ionel_domnita@yahoo.com>
2020-11-15 17:45:37 +0200

    lab6

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Domnita_Ionel_Lab5.Models;
+
+
+ namespace Domnita_Ionel_Lab5.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Expense> Expense { get; set; }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; internal set; }
+        public DbSet<Budget> Budget { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Domnita_Ionel_Lab5.Models;
+using Microsoft.OpenApi.Models;
+
+namespace Domnita_Ionel_Lab5
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ExpenseContext>(opt =>
+                opt.UseInMemoryDatabase("ExpenseList"));
+            services.AddControllers();
+            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Expenses API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://econ.ubbcluj.ro/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Nume Prenume",
+                        Email = string.Empty,
+                        Url = new Uri("https://econ.ubbcluj.ro/cv.php?id=540"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://econ.ubbcluj.ro/licence"),
+                    }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Expenses API v1.1");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
